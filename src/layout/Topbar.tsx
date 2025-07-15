925dc479d0f3e5a93f03d4e4c00a0acf08d2e353
zhui <zhui813113@gmail.com>
2025-07-15 12:47:24 +0800

    feat: hidden login button if mobile

--- src/layout/Topbar.tsx
+++ src/layout/Topbar.tsx
@@ -2,22 +2,37 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { LogOut } from "react-feather";
 import { Link } from "react-router";
+import logo from "../assets/logo.png";
 // Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css";
+import { useEffect, useState } from "react";
 
 export function Topbar() {
   const { publicKey, disconnect } = useWallet();
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
 
   return (
     <div className="mt-8 mb-4 flex w-full justify-between px-2">
       <Link to="/">
-        <img src="/logo.png" alt="Logo" className="mx-6 h-20" />
+        <img src={logo} alt="Logo" className="mx-6 h-20" />
       </Link>
 
-      <div className="mr-2 flex items-center">
-        <WalletMultiButton />
+      <div hidden={isMobile} className="mr-2 flex items-center">
+        <WalletMultiButton className=" bg-amber-600" />
         {publicKey && <LogOut onClick={()=>{disconnect()}} className="m-2 cursor-pointer text-white" />}
       </div>
     </div>
   );
 }
+
